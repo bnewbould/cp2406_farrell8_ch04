10498b1dbd088daf74f310fd4529532c9be5594b
jc210391 <benjamin.newbould@my.jcu.edu.au>
2017-08-18 15:17:26 +1000

    fixed

---
+++ DebuggingExercises/FixDebugBox.java
@@ -0,0 +1,26 @@
+public class FixDebugBox {
+    private int width;
+    private int length;
+    private int height;
+
+    public FixDebugBox() {
+        length = 1;
+        width = 1;
+        height = 1;
+    }
+
+    public FixDebugBox(int width, int length, int height) {
+        this.width = width;
+        this.length = length;
+        this.height = height;
+    }
+
+    public void showData() {
+        System.out.println("Width: " + width + "  Length: " +
+                length + "  Height: " + height);
+    }
+
+    public double getVolume() {
+        return (double) (length * width * height);
+    }
+}
